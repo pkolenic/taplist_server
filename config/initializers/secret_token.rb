647b93cef0d091ae41c55dd6ebd892d6fef60385
Patrick Kolenic <pkolenic@gmail.com>
2013-07-23 21:43:25 -0600

    Switch to Rails 4.0 and Ruby 2.0

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Taplist::Application.config.secret_token = 'ffb434b4409e3cb4ff3d45eedae99a94f797625e4e51a8337b4aaf954acfaa91d1f117c4992f29118adc5b6ff891ec8a51023a34e29d114a6ac1ec6cdc14125b'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Taplist::Application.config.secret_key_base = '8d3d1815dc10f0558831ad1e7f56b45dff24ee59d52e0eb09e293c861459aba50d5f475da66f20e85e12c738d7c78e79dfbc8826d9d402364ae59712d79721fc'
